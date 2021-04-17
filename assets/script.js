2cbaed9171241d35df8343a5e18fcfdf17255b98
Jason Manning <jasonkylemanning@gmail.com>
2021-04-17 13:34:46 -0400

    update js file

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,16 @@ function nextQuestion () {
 
 function showQuestion(question) {
     questionElem.innerText = question.question
+    question.answer.forEach( answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonElem.appendChild("button")
+    })
 }
 
 function selectAnswer () {
