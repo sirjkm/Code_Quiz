255a9d8d28ae6e764061b558e296492204fdc6da
Jason Manning <jasonkylemanning@gmail.com>
2021-04-17 12:03:44 -0400

    update html, css, js files

--- assets/script.js
+++ assets/script.js
@@ -1,77 +1,65 @@
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var startButton = document.getElementById("startbtn");
+var questionContainerElem = document.getElementById("question-box");
+var questionElem = document.getElementById("question")
+var answerButtonElem = document.getElementById("answerbtn")
+
+startButton.addEventListener("click", startGame)
+
+function startGame () {
+    startButton.classList.add("hide")
+    shuffleQuestion = question.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElem.classList.remove("hide")
+    nextQuestion()
+}
+
+function nextQuestion () {
+    showQuestion(shuffleQuestion[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElem.innerText = question.question
+}
+
+function selectAnswer () {
+
+}
 
 var questions = [
     {
         question: "What does HTML stand for?",
-        a: "Hot Tacos Make Life",
-        b: "Hypertext Markup Language",
-        c: "Have the money, Lance!",
-        answer: "B"
+        answers: [
+            { text: "Hot Tacos Make Life", correct: false },
+            { text: "Hypertext Markup Language", correct: true },
+            { text: "Have the money, Lance!", correct: false },
+            { text: "Hypertext Makeup Language", correct: false }
+        ]
     },
     {
         question: "How much maths do you know?",
-        a: "A lot!",
-        b: "I can hold my own with my iPhone close by",
-        c: "Who is maths?",
-        answer: "C"
+        answers: [
+            { text: "A lot!", correct: false },
+            { text: "I can hold my own with my iPhone close by", correct: false },
+            { text: "Who is maths?", correct: false },
+            { text: "Math is life", correct: true }
+        ]
     },
     {
         question: "Mac or PC?",
-        a: "Mac",
-        b: "PC",
-        c: "The Stone Tablets of Moses",
-        answer: "A"
+        answers: [
+            { text: "Mac", correct: true },
+            { text: "PC", correct: false },
+            { text: "The Stone Tablets of Moses", correct: false },
+            { text: "Abacus & a Whiteclaw", correct: false }
+        ]
     },
     {
         question: "Fixing mistakes in a program is called?",
-        a: "Sequencing",
-        b: "Looping",
-        c: "Debugging",
-        answer: "C"
+        answers: [
+            { text: "Debugging", correct: true },
+            { text: "Looping", correct: false },
+            { text: "Sequencing", correct: false },
+            { text: "Dancing with Death", correct: false },
+        ]
     }
     ];
-
-function get(x){
-    return document.getElementById(x);
-}
-
-function renderQuestion(){
-    test = get("test");
-    if(pos >= questions.length){
-    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
-    pos = 0;
-    correct = 0;
-    return false;
-    }
-    get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
-    question = questions[pos].question;
-    chA = questions[pos].a;
-    chB = questions[pos].b;
-    chC = questions[pos].c;
-    test.innerHTML = "<h3>"+question+"</h3>";
-
-    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
-    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-}
-
-function checkAnswer(){
-    choices = document.getElementsByName("choices");
-    for(var i=0; i<choices.length; i++){
-    if(choices[i].checked){
-        choice = choices[i].value;
-    }
-    }
-
-    if(choice == questions[pos].answer){
-    correct++;
-    }
-
-    pos++;
-
-    renderQuestion();
-}
-
-window.addEventListener("load", renderQuestion);
